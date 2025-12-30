a4bd7ac11ee93720d8696d8eb8603f60008dff55
frankferrari <reg.drehkopf@posteo.de>
2025-12-30 19:58:26 +0100

    ADD Controller logic, Views (styled), and Routes

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,45 @@
+class GamesController < ApplicationController
+  def index
+    @games = Game.order(votes: :desc, name: :asc)
+    @new_game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to root_path, notice: "Game added successfully."
+    else
+      @games = Game.order(votes: :desc, name: :asc)
+      @new_game = @game
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @game = Game.find(params[:id])
+    @game.destroy
+    redirect_to root_path, notice: "Game removed."
+  end
+
+  def vote
+    @game = Game.find(params[:id])
+    direction = params[:direction] == "down" ? -1 : 1
+
+    # Ensure votes don't go below 0
+    new_votes = [ @game.votes + direction, 0 ].max
+
+    @game.update(votes: new_votes)
+    redirect_to root_path
+  end
+
+  def reset
+    Game.update_all(votes: 0)
+    redirect_to root_path, notice: "All votes have been reset."
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :genre, :price, :max_players)
+  end
+end
